abc6a90b82f61cc320028f7223bfc071efec3c37
Matheus Levi <matheuslevit@gmail.com>
2025-07-22 21:07:01 -0300

    fix strapi database config

--- apps/backend/config/database.ts
+++ apps/backend/config/database.ts
@@ -21,30 +21,20 @@ export default ({ env }) => {
       pool: { min: 2, max: 10 },
     },
     postgres: {
-      connection: env("DATABASE_URL")
-        ? {
-            connectionString: env("DATABASE_URL"),
-            ssl: env.bool("DATABASE_SSL", false) && {
-              rejectUnauthorized: env.bool(
-                "DATABASE_SSL_REJECT_UNAUTHORIZED",
-                false
-              ),
-            },
-          }
-        : {
-            host: env("DATABASE_HOST", "localhost"),
-            port: env.int("DATABASE_PORT", 5432),
-            database: env("DATABASE_NAME", "strapi"),
-            user: env("DATABASE_USERNAME", "strapi"),
-            password: env("DATABASE_PASSWORD", "strapi"),
-            ssl: env.bool("DATABASE_SSL", false) && {
-              rejectUnauthorized: env.bool(
-                "DATABASE_SSL_REJECT_UNAUTHORIZED",
-                false
-              ),
-            },
-            schema: env("DATABASE_SCHEMA", "public"),
-          },
+      connection: {
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        ssl: env.bool("DATABASE_SSL", false) && {
+          rejectUnauthorized: env.bool(
+            "DATABASE_SSL_REJECT_UNAUTHORIZED",
+            false
+          ),
+        },
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
       pool: { min: 2, max: 10 },
     },
     sqlite: {
