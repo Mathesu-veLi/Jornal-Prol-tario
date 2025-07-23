7082d1ebb716b6f4f93eefa77498d5cf1dda80ac
Matheus Levi <matheuslevit@gmail.com>
2025-07-22 21:51:58 -0300

    fix post type

--- apps/frontend/src/types/post.ts
+++ apps/frontend/src/types/post.ts
@@ -1,5 +1,18 @@
 export interface Post {
   documentId: number;
+  cover: Media;
   title: string;
   text: string;
+  description: string;
+}
+
+interface Media {
+  id: number;
+  url: string;
+  formats?: {
+    thumbnail?: { url: string };
+    small?: { url: string };
+    medium?: { url: string };
+    large?: { url: string };
+  };
 }
