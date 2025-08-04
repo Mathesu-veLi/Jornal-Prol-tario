f9d27416b8ab012a1c7be250ccd13b20a0daf169
Matheus Levi <matheuslevit@gmail.com>
2025-08-04 11:55:58 -0300

    shows the last article in the top of website

--- apps/frontend/src/pages/PostsList.tsx
+++ apps/frontend/src/pages/PostsList.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import { api, baseUrl } from "../api";
-import { type Post } from "../types/post";
 import { Link } from "react-router-dom";
+import { api, baseUrl } from "../api";
+import { useEffect, useState } from "react";
+import type { Post } from "../types/post";
 
-function PostsList() {
+export default function PostsList() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(true);
 
@@ -16,34 +16,56 @@ function PostsList() {
 
   if (loading) return <p>Carregando...</p>;
 
+  const lastPost = posts[posts.length - 1];
+  const otherPosts = posts.slice(0, posts.length - 1);
+
   return (
     <div className="container">
+      <img src="/favicon.ico" alt="Icon" />
+
       <h2>Últimas Notícias</h2>
+
+      {lastPost && (
+        <div className="post last-post">
+          <Link to={`/post/${lastPost.documentId}`}>
+            {lastPost.cover && (
+              <div className="cover-wrapper">
+                <img
+                  src={`${baseUrl}${lastPost.cover.url}`}
+                  alt={lastPost.cover.alternativeText || lastPost.title}
+                  width="100%"
+                />
+                <h2 className="cover-title">{lastPost.title}</h2>
+              </div>
+            )}
+            <span>{lastPost.description}</span>
+            <br />
+            <span className="button">Ler mais</span>
+          </Link>
+        </div>
+      )}
+
       <div className="posts">
-        {posts.map((post) => (
+        {otherPosts.map((post) => (
           <article key={post.documentId} className="post">
             <Link to={`/post/${post.documentId}`}>
               {post.cover && (
                 <img
                   src={`${baseUrl}${post.cover.url}`}
-                  alt={post.cover.alternativeText}
-                  width={"100%"}
+                  alt={post.cover.alternativeText || post.title}
+                  width="100%"
                 />
               )}
-              <h3>
-                <Link to={`/post/${post.documentId}`}>{post.title}</Link>
-              </h3>
+              <h3>{post.title}</h3>
               <span>{post.description}</span>
               <br />
-              <Link to={`/post/${post.documentId}`} className="button">
-                Ler mais
-              </Link>
+              <span className="button">Ler mais</span>
             </Link>
           </article>
         ))}
+
+        {/* Última notícia em destaque */}
       </div>
     </div>
   );
 }
-
-export default PostsList;
